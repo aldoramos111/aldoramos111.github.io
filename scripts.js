0da84d44af7e5f7774bd4aa4e5f5e381c04c3835
aldoramos111 <55818494+aldoramos111@users.noreply.github.com>
2021-08-03 11:02:51 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 //Swiper
 const swiper = new Swiper('.swiper-container', {
-  effect: 'fade',
-  fadeEffect: {
-    crossFase: true
+  effect: 'coverflow',
+  coverflowEffect: {
+    depth: 100
   },
   preventInteractionOnTransition: false,
   speed: 800,
