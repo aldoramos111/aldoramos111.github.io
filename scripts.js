a343f613da1e39b75d12084d2ffdda853e249642
Aldo <aldoramos.human@gmail.com>
2021-08-23 01:29:04 -0700

    no message

--- scripts.js
+++ scripts.js
@@ -39,11 +39,11 @@ $(document).ready(function() {
         StopEmbedVideo();
     });
 });
-
+/*
 var iframe = document.getElementById('embedvideo');
 var player = $f(iframe);
 var playButton = document.getElementById("closevid");
 playButton.addEventListener("click", function() {
     player.api("play");
-});
+});*/
 
