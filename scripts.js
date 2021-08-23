82ed5f3aec0016a7914525a6ba1d2479e670e551
Aldo <aldoramos.human@gmail.com>
2021-08-22 23:29:55 -0700

    no message

--- scripts.js
+++ scripts.js
@@ -1,24 +1,3 @@
-//Swiper
-const swiper = new Swiper('.swiper-container', {
-    effect: 'fade',
-    fadeEffect: {
-        crossFade: true
-    },
-    preventInteractionOnTransition: false,
-    speed: 800,
-    spaceBetween: 100,
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-    },
-    loop: true,
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false
-    },
-});
-
 //Mobile Nav Toggle
 function mobileNavToggle() {
     var element = document.getElementById("navList");
