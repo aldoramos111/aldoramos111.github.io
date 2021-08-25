81b38ac03aad4fe74abacc5d619cce99f459e8c0
Aldo <aldoramos.human@gmail.com>
2021-08-25 00:10:09 -0700

    no message

--- vi-scripts.js
+++ vi-scripts.js
@@ -4,3 +4,23 @@ var playButton = document.getElementById("closevid");
 playButton.addEventListener("click", function() {
     player.api("play");
 });
+
+function StopEmbedVideo() {
+    var $frame = $('iframe#embedvideo');
+
+    // saves the current iframe source
+    var vidsrc = $frame.attr('src');
+
+    // sets the source to nothing, stopping the video
+    $frame.attr('src', '');
+
+    // sets it back to the correct link so that it reloads immediately on the next window open
+    $frame.attr('src', vidsrc);
+
+};
+
+$(document).ready(function() {
+    $('.modal').click(function() {
+        StopEmbedVideo();
+    });
+});
