d8397aa376677ee6857a82ed5c1a7cf1599e5078
Aldo <aldoramos.human@gmail.com>
2021-08-25 00:04:52 -0700

    no message

--- scripts.js
+++ scripts.js
@@ -20,24 +20,4 @@ $(window).scroll(function() {
     }
 });
 
-function StopEmbedVideo() {
-    var $frame = $('iframe#embedvideo');
-
-    // saves the current iframe source
-    var vidsrc = $frame.attr('src');
-
-    // sets the source to nothing, stopping the video
-    $frame.attr('src', '');
-
-    // sets it back to the correct link so that it reloads immediately on the next window open
-    $frame.attr('src', vidsrc);
-
-};
-
-$(document).ready(function() {
-    $('.modal').click(function() {
-        StopEmbedVideo();
-    });
-});
-
 
